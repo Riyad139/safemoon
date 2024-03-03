a1a33cc472e3dbe1fd2f8021555829533912d817
Riyad139 <riyad.ahmed1886@gmail.com>
2024-03-03 09:38:07 +0600

    fixed ?

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,25 @@ import { FaXTwitter } from "react-icons/fa6";
 export default function Home() {
   return (
     <main className="text-white min-h-screen relative   ">
-      <video
+      {/* <video
         src="/bag1.mp4"
         className="min-h-[100%] -z-50 brightness-75 absolute  min-w-[100%] object-cover"
         autoPlay={true}
         muted={true}
         loop={true}
         playsInline
-      />
-      {/* <video
+      /> */}
+      <video
         className="min-h-[100%] -z-50 brightness-75 absolute  min-w-[100%] object-cover"
         loop={true}
         playsInline
         autoPlay={true}
         muted={true}
+        controls
       >
+        <source src="/bag1.mp4" type="video/mp4" />
         <source src="/bag.webm" type="video/webm" />
-        <source src="/bag.mp4" type="video/mp4" />
-      </video> */}
+      </video>
       {/* <div className="flex justify-between px-10 py-8">
         <div className="flex space-x-5">
           <p className="border-r px-3">90 queen St melbounce</p>
