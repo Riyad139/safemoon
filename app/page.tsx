4dc2017d1b1e78d8f8853b767c96ce8d0be17fef
Riyad139 <riyad.ahmed1886@gmail.com>
2024-03-04 18:15:35 +0600

    add animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
 import Headers from "@/components/Headers";
+import TheMoonAnimation from "@/utils/MoonAnimation";
 import Image from "next/image";
-import { FaFacebook } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <main className="text-white min-h-screen relative   ">
-      <Image
-        src={"/bag.gif"}
-        width={500}
-        className="min-h-[100%] -z-50 brightness-75 absolute  min-w-[100%] object-cover"
-        height={500}
-        alt="background"
-      />
+    <main className="text-white min-h-screen relative">
+      <div className="absolute -z-40 w-full h-full">
+        <TheMoonAnimation />
+      </div>
       <Headers />
     </main>
   );
---
+++ utils/MoonAnimation/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Spline from "@splinetool/react-spline";
+export default function TheMoonAnimation() {
+  return (
+    <Spline
+      className="relative -z-30"
+      scene="https://prod.spline.design/ENx0NPkY2NPNPB4u/scene.splinecode"
+      //scene="https://prod.spline.design/ENx0NPkY2NPNPB4u/scene.splinecode"
+    />
+  );
+}
