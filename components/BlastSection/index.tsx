45bbc0de22b30012972ff4236e62a83a61504bfc
Riyad139 <riyad.ahmed1886@gmail.com>
2024-03-04 23:43:52 +0600

    fixed

--- components/BlastSection/index.tsx
+++ components/BlastSection/index.tsx
@@ -1,16 +1,24 @@
+import RoundAnimation from "@/utils/RoundAnimation";
+
 export default function BlastSection() {
   return (
-    <div className="uppercase max-w-[60rem] px-5 text-center mx-auto py-36 text-white">
-      <h3 className=" text-4xl sm:text-6xl border-t-4 pt-11 font-semibold">
-        Blast your projects with our All-in-one tool
-      </h3>
-      <p className="mt-7 text-base sm:text-xl">
-        Introducing the most comprehensive DEX on BLAST! {"We've"} built a
-        user-friendly platform that empowers anyone to launch their projects on
-        this blockchain without the need for programming skills. Your project,
-        your way <br /> – unleash your creativity effortlessly with our
-        all-inclusive DEX.
-      </p>
+    <div className="text-white w-full min-h-[50vh] relative">
+      <div className="absolute h-full w-full -z-20">
+        <RoundAnimation />
+      </div>
+
+      <div className="uppercase max-w-[60rem] relative px-5 text-center mx-auto py-36 text-white">
+        <h3 className=" text-4xl sm:text-6xl border-t-4 pt-11 font-semibold">
+          Blast your projects with our All-in-one tool
+        </h3>
+        <p className="mt-7 text-base sm:text-xl">
+          Introducing the most comprehensive DEX on BLAST! {"We've"} built a
+          user-friendly platform that empowers anyone to launch their projects
+          on this blockchain without the need for programming skills. Your
+          project, your way <br /> – unleash your creativity effortlessly with
+          our all-inclusive DEX.
+        </p>
+      </div>
     </div>
   );
 }
---
+++ utils/RoundAnimation/index.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Spline from "@splinetool/react-spline";
+export default function RoundAnimation() {
+  return (
+    <Spline
+      className="w-full"
+      scene="https://prod.spline.design/MF4-bWojeaozaSLP/scene.splinecode"
+    />
+  );
+}
