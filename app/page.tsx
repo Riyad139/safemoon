f707c916fe42a6e8ce887dc8cfa278f76c9f53e4
Riyad139 <riyad.ahmed1886@gmail.com>
2024-03-04 19:47:53 +0600

    added

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,6 @@ export default function Home() {
     <main className="text-white w-full min-h-screen relative">
       <div className="absolute -z-40 w-full h-full">
         <TheMoonAnimation />
-        <Image
-          src={"/moon.png"}
-          alt="moon"
-          width={500}
-          height={500}
-          quality={100}
-          className="w-[100%] h-full object-cover absolute -z-50 "
-        />
       </div>
       <Headers />
     </main>
--- utils/MoonAnimation/index.tsx
+++ utils/MoonAnimation/index.tsx
@@ -4,10 +4,7 @@ import Image from "next/image";
 export default function TheMoonAnimation() {
   return (
     <div className="w-full ">
-      <Spline
-        className="-z-30 absolute"
-        scene="https://prod.spline.design/aY63NfAHCZjF1sm8/scene.splinecode"
-      />
+      <Spline scene="https://prod.spline.design/V-gLmuZzOhHTDX0R/scene.splinecode" />
     </div>
   );
 }
