b175225bddf8984557c0faf64f7c56c69402d4a5
Riyad139 <riyad.ahmed1886@gmail.com>
2024-03-04 19:03:49 +0600

    add animations

--- utils/MoonAnimation/index.tsx
+++ utils/MoonAnimation/index.tsx
@@ -1,11 +1,21 @@
 "use client";
 import Spline from "@splinetool/react-spline";
+import Image from "next/image";
 export default function TheMoonAnimation() {
   return (
-    <Spline
-      className="relative -z-30"
-      scene="https://prod.spline.design/ENx0NPkY2NPNPB4u/scene.splinecode"
-      //scene="https://prod.spline.design/ENx0NPkY2NPNPB4u/scene.splinecode"
-    />
+    <div>
+      <Image
+        src={"/moon.png"}
+        alt="moon"
+        width={500}
+        height={500}
+        quality={100}
+        className="w-full absolute -z-50 h-full"
+      />
+      <Spline
+        className="-z-30 absolute"
+        scene="https://prod.spline.design/aY63NfAHCZjF1sm8/scene.splinecode"
+      />
+    </div>
   );
 }
