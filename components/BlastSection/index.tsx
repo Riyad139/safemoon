2fc453ebd194d4886a47f4c7e8dfbc129f14e705
Riyad139 <riyad.ahmed1886@gmail.com>
2024-03-04 21:26:33 +0600

    fixed

--- components/BlastSection/index.tsx
+++ components/BlastSection/index.tsx
@@ -1,10 +1,10 @@
 export default function BlastSection() {
   return (
-    <div className="uppercase max-w-[60rem]  text-center mx-auto py-36 text-white">
-      <h3 className="text-6xl border-t-4 pt-11 font-semibold">
+    <div className="uppercase max-w-[60rem] px-5 text-center mx-auto py-36 text-white">
+      <h3 className=" text-4xl sm:text-6xl border-t-4 pt-11 font-semibold">
         Blast your projects with our All-in-one tool
       </h3>
-      <p className="mt-7 text-xl">
+      <p className="mt-7 text-base sm:text-xl">
         Introducing the most comprehensive DEX on BLAST! {"We've"} built a
         user-friendly platform that empowers anyone to launch their projects on
         this blockchain without the need for programming skills. Your project,
--- components/Headers/index.tsx
+++ components/Headers/index.tsx
@@ -6,7 +6,7 @@ export default function Headers() {
   const headerText = useTypingEffect("SAFEMOON");
   return (
     <div className="w-full px-5 text-center pt-28 sm:pt-48">
-      <h2 className="text-8xl font-bold">{headerText}</h2>
+      <h2 className=" text-6xl sm:text-8xl font-bold">{headerText}</h2>
       <p className="text-2xl pt-9">NEW BULL RUN • SAME MISSION • NEW TEAM</p>
       <button className="uppercase bg-[#FCFC03] text-black px-5 py-3 text-xl  mt-8 ">
         Launch App
