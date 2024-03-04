13b6b5e249950fb7e4d524f5a3e8d52e5b2d89ae
Riyad139 <riyad.ahmed1886@gmail.com>
2024-03-05 00:26:35 +0600

    added

--- components/BlastSection/index.tsx
+++ components/BlastSection/index.tsx
@@ -1,10 +1,17 @@
 import RoundAnimation from "@/utils/RoundAnimation";
+import Image from "next/image";
 
 export default function BlastSection() {
   return (
     <div className="text-white w-full min-h-[50vh] relative">
-      <div className="absolute h-full w-full -z-20">
-        <RoundAnimation />
+      <div className="absolute flex justify-center h-full w-full -z-20">
+        <Image
+          src={"/donut.gif"}
+          className=" object-cover h-full"
+          width={500}
+          height={500}
+          alt=""
+        />
       </div>
 
       <div className="uppercase max-w-[60rem] relative px-5 text-center mx-auto py-36 text-white">
--- components/Headers/index.tsx
+++ components/Headers/index.tsx
@@ -7,7 +7,9 @@ export default function Headers() {
   return (
     <div className="w-full px-5 text-center pt-28 sm:pt-48">
       <h2 className=" text-6xl sm:text-8xl font-bold">{headerText}</h2>
-      <p className="text-2xl pt-9">NEW BULL RUN • SAME MISSION • NEW TEAM</p>
+      <p className="text-2xl uppercase pt-9">
+        New Bullrun • New Safemoon • Same Mission
+      </p>
       <button className="uppercase bg-[#FCFC03] text-black px-5 py-3 text-xl  mt-8 ">
         Launch App
       </button>
