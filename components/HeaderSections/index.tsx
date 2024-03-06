3c3f716571318376fe61c080a628583b1be46274
Riyad139 <riyad.ahmed1886@gmail.com>
2024-03-07 04:55:32 +0600

    added

--- components/HeaderSections/index.tsx
+++ components/HeaderSections/index.tsx
@@ -1,15 +1,9 @@
-"use client";
 import TheMoonAnimation from "@/utils/MoonAnimation";
 import Headers from "../Headers";
-import useTypingEffect from "@/hooks/useTypingEffect";
 export default function HeaderSections() {
-  const headerText = useTypingEffect("SAFEMOON");
   return (
     <main className="text-white w-full min-h-screen relative">
-      <div className="absolute  -top-[10%] -z-40 w-full h-full">
-        <h2 className=" text-6xl top-[38%] absolute z-40 left-[50%] -translate-x-[50%] -translate-y-[50%] mt-28 sm:text-8xl font-bold">
-          {headerText}
-        </h2>
+      <div className="absolute -top-36 -z-40 w-full h-full">
         <TheMoonAnimation />
       </div>
       <Headers />
--- components/Headers/index.tsx
+++ components/Headers/index.tsx
@@ -5,9 +5,32 @@ import { FaXTwitter } from "react-icons/fa6";
 import Link from "next/link";
 
 export default function Headers() {
+  const headerText = useTypingEffect("SAFEMOON");
   return (
     <div className="w-full  px-5 text-center pt-28  min-h-screen sm:pt-48">
-      <div className="relative mt-[140%] sm:mt-[68%] md:mt-[53%] lg:mt-[40%]">
+      <div className="">
+        <h2 className=" text-6xl mt-28 sm:text-8xl font-bold">{headerText}</h2>
+        <p className="text-2xl uppercase pt-9">
+          New Bullrun • New Safemoon • Same Mission
+        </p>
+        <div className="flex justify-center gap-5">
+          <Link
+            target="_blank"
+            href={"https://t.me/SafemoonComeback"}
+            className="uppercase bg-[#FCFC03] text-black px-3 py-1.5 rounded-sm text-xl  mt-8 "
+          >
+            <BiLogoTelegram size={30} />
+          </Link>
+          <Link
+            target="_blank"
+            href={"https://x.com/safemoonsol2024?s=21&t=omkKmJifOvtNJO3oM38jiw"}
+            className="uppercase bg-[#FCFC03] text-black px-3 py-1.5 rounded-sm text-xl  mt-8 "
+          >
+            <FaXTwitter size={30} />
+          </Link>
+        </div>
+      </div>
+      <div className="relative mt-[50%] sm:mt-[45%] md:mt-[35%] lg:mt-[23%]">
         <div className="flex mb-7 justify-center gap-4">
           <Link
             target="_blank"
